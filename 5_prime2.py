5722a4937831d49f9c3b1fe66b1a3e719478e83e
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-19 21:01:41 +0530

    Add files via upload

---
+++ 5_prime2.py
@@ -0,0 +1,10 @@
+# Prime numbers within range---------
+
+# j = 1
+# for i in range(a, b):
+#     j++
+#     if i % j == 0:
+#         print("Not Prime------")
+#         break
+# else:
+#     print("Prime------")
