1b6d7dd40c01844527289443bf9b559ab484eef2
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-09-19 21:00:34 +0530

    Add files via upload

---
+++ 2_abc.py
@@ -0,0 +1,25 @@
+# a,b,c=input("Enter three values of a, b and c respectively: ").split()
+
+# print("The value of a: ",a)
+# print("The value of b: ",b)
+# print("The value of c: ",c)
+
+
+
+
+
+# a,b,c=map(int, input("Enter three values of a, b and c respectively: ").split())
+# print("The value of a1: ",a)
+# print("The value of b1: ",b)
+# print("The value of c1: ",c)
+
+
+
+
+
+
+
+a,b,c=input("x: "),input("y: "), input("z: ")
+print("The value of a2: ",a)
+print("The value of b2: ",b)
+print("The value of c2: ",c)
